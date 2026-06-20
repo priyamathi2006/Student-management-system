36d5b62a6ec4f308ece1f356c3cdcd325a668d0b
Mohana priya <pririyamathi@gmail.com>
2026-06-20 13:41:39 +0530

    fix vercel build

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
-import login from './pages/login.jsx';
+import Login from './pages/login.jsx';
 import Layout from './Components/Layout.jsx';
 
 import Studentlist from './pages/Studentlist.jsx';
@@ -14,31 +14,29 @@ const App = () => {
   return (
     <Routes>
 
-      {/* login Route */}
-      <Route path="/login" element={<login />} />
+      {/* Login Route */}
+      <Route path="/login" element={<Login />} />
 
       {/* Main Layout */}
       <Route path="/" element={<Layout />}>
-
-        {/* Redirect / to /students */}
+        
+        {/* Default Route */}
         <Route index element={<Navigate to="/login" replace />} />
 
         {/* Student Routes */}
-        <Route path="student" element={<Studentlist />} />
         <Route path="students" element={<Studentlist />} />
         <Route path="students/add" element={<Studentform />} />
         <Route path="students/edit/:id" element={<Studentform />} />
 
         {/* Subject Routes */}
-        <Route path="subject" element={<Subjectlist />} />
         <Route path="subjects" element={<Subjectlist />} />
         <Route path="subjects/add" element={<Subjectform />} />
         <Route path="subjects/edit/:id" element={<Subjectform />} />
 
       </Route>
 
-      {/* If route not found, redirect to / */}
-      <Route path="*" element={<Navigate to="/" replace />} />
+      {/* Invalid Route */}
+      <Route path="*" element={<Navigate to="/login" replace />} />
 
     </Routes>
   );
